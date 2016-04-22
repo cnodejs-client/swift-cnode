d7239ea695174214a45d31ce447e38ae14b4e539
Feng Chen <chenf@outlook.com>
2016-04-22 11:39:01 +0800

    fix avatar image display

--- swift-cnode/Author.swift
+++ swift-cnode/Author.swift
@@ -10,7 +10,14 @@ import Foundation
 
 class Author : PPJSONSerialization {
     var loginname: String?
-    var avatar_url: String?
+    var avatar_url: String? {
+        didSet {
+            // 如果用户的是用的gravatar头像，那么通过api获取到的avatar_url会缺少开头的"https:"，导致头像无法正确加载
+            if avatar_url!.hasPrefix("//") {
+                avatar_url = "https:" + avatar_url!
+            }
+        }
+    }
     var githubUsername: String?
     var create_at: NSDate?
     var recent_topics: [Topic] = []
